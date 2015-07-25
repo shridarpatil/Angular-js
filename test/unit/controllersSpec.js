8894970f700551a5121121dd766f9e5153f108ac
shridarpatil <shridharpatil2792@gmail.com>
2015-07-25 11:51:55 +0530

    first commit

---
+++ test/unit/controllersSpec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('controllers', function() {
+beforeEach(module('phonecatApp'));
+   beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('phones/phones.json').
+        respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
+    scope = $rootScope.$new();
+    ctrl = $controller('PhoneListCtrl', {$scope: scope});
+    }));
+
+    it('should create "phones" model with 3 phones', function() {
+      expect(scope.phones.length).toBe(3);
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
+});
